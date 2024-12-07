835feea6c9975afba37f6c6bfce7821dcc361719
luqmansen <luqmansen@gmail.com>
2024-12-07 21:31:00 +0100

    setup test

---
+++ src/add.c
@@ -0,0 +1,9 @@
+#include "add.h"
+
+int add(const int a, const int b) {
+    return a + b;
+}
+
+int sub(const int a, const int b) {
+    return a - b;
+}
---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "add.h"
+
+
+int main(void)
+{
+    printf("Hello, World!\n");
+    return add(1, 1);
+}
